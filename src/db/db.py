f765efcba8c1bff47d860b33f7a75b8e60b5784c
Tanmay Khairnar <tanmayska11@gmail.com>
2026-06-16 13:03:26 +0200

    data base change to supabase and db.py changed

--- src/db/db.py
+++ src/db/db.py
@@ -1,11 +1,15 @@
 from sqlalchemy import create_engine
-from src.config import DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME
-
+from src.config import (
+    DB_USER,
+    DB_PASSWORD,
+    DB_HOST,
+    DB_PORT,
+    DB_NAME
+)
 
 def get_engine():
-    
     return create_engine(
         f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}",
-        pool_pre_ping=True
+        pool_pre_ping=True,
+        connect_args={"sslmode": "require"}
     )
-    
